53c55b582bf7e8aa1822df9d9edb064ad099015b
Hugo <827920340@qq.com>
2025-12-26 17:47:55 +0800

    fix: 修正 update 函數中 sql 變數拼寫的錯誤

--- todoModel.js
+++ todoModel.js
@@ -12,6 +12,13 @@ const TodoModel = {
     const result = await pool.query(sql, [title]);
     return result.rows[0];
   },
+
+  async update(id, title, is_completed) {
+    const sql =
+      "UPDATE todos SET title = $1, is_completed = $2 WHERE id = $3 RETURNING *";
+    const result = await pool.query(sql, [title, is_completed, id]);
+    return result.rows[0];
+  },
 };
 
 module.exports = TodoModel;
