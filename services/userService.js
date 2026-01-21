bf60b0001ccf33ba7126c2be176cf71059bead36
Hugo <827920340@qq.com>
2026-01-21 17:29:16 +0800

    feat: 引入bcrypt工具將用戶密碼加密儲存

--- services/userService.js
+++ services/userService.js
@@ -1,20 +1,24 @@
 const userModel = require("../models/userModel");
+const bcrypt = require("bcrypt");
 
 const userService = {
   registerUser: async (username, password) => {
     const existingUser = await userModel.findByUsername(username);
-    if (existingUser) {
-      throw new Error("用戶名已被使用");
-    }
-    const newUser = await userModel.create(username, password);
+    if (existingUser) throw new Error("用戶名已被使用");
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await userModel.create(username, hashedPassword);
     return { id: newUser.id, username: newUser.username };
   },
 
   loginUser: async (username, password) => {
     const user = await userModel.findByUsername(username);
-    if (!user || user.password !== password) {
-      throw new Error("用戶名不存在或密碼不正確");
-    }
+    if (!user) throw new Error("用戶名不存在或密碼錯誤");
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) throw new Error("用戶名不存在或密碼錯誤");
+
     return { id: user.id, username: user.username };
   },
 };
